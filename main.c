a4020c72265d5e0e8b4420cbc2289b610f839cda
Adam Jagielski <jagielski.adamm@gmail.com>
2020-11-05 16:01:50 +0100

    Added data-structures, random generation and printing  +Added Solution, Clause and CNF-formula data structs  +Added random generation for above structs  +Added printing of above structs

--- main.c
+++ main.c
@@ -2,182 +2,132 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-int* solve_sat(int** sat);
+#include <stdint.h>
 
-int evalute_solution(int** sat, int* solution);
-int* random_solution(int var_num);
-int** random_ksat(int var_num, int cls_num, int k);
+//#######################//
 
-void erase_solution(int* solution);
-void erase_sat(int** sat);
+struct solution{
+    uint64_t size;
+    uint64_t length;
+    uint64_t residue;
+    uint64_t* value;
+    uint64_t* presence;
+};
+struct clause{
+    uint64_t size;
+    uint64_t length;
+    uint64_t residue;
+    uint64_t* value;
+    uint64_t* presence;
+};
+struct sat{
+    uint64_t cls_num;
+    uint64_t var_num;
+    struct clause** clauses;
+};
 
-void print_array(int* array);
-void print_formula(int** formula);
+//#######################//
 
-//###################################################//
+struct solution* random_solution(uint64_t size);
+struct clause* random_clause(uint64_t size);
+struct sat* random_sat(uint64_t cls_num, uint64_t var_num);
+
+void print_solution(struct solution* sol);
+void print_clause(struct clause* sol);
+void print_sat(struct sat* formulae);
+
+uint64_t rand64();
+
+//#######################//
 
 int main(int argc, char** argv){
-    srand(time(NULL));
-    int** sat = random_ksat(100,420,3);
-    int* sol = solve_sat(sat);
-    //print_formula(sat);
-    print_array(sol);
-    printf("%d\n",evalute_solution(sat,sol));
-    erase_sat(sat);
-    erase_solution(sol);
+    srand(0);
+    struct sat* formulae = random_sat(8,81);
+    print_sat(formulae);
 }
 
-//###################################################//
+//#######################//
 
-int* solve_sat(int** sat){
-    
-    int var_num = sat[0][0];
-    int cls_num = sat[0][1];
-    int* current_solution = random_solution(var_num);
-    int current_score = evalute_solution(sat, current_solution);
-
-    double MAX_RETRIES = 1000;
-    double MAX_TEMPERATURE = 0.3;
-    double MIN_TEMPERATURE = 0.001;
-    double DECAY_RATE = 1.0 / var_num;
-    double current_temperature = MAX_TEMPERATURE;
-
-    int i;
-    for(i = 0; i < MAX_RETRIES; i++){
-        int j = 0;
-        while(current_temperature >= MIN_TEMPERATURE){
-            current_temperature = MAX_TEMPERATURE * pow(M_E, (-j)*DECAY_RATE);
-            int k;
-            for(k = 1; k <= var_num ; k++){
-                current_solution[k] = -current_solution[k];
-                int new_score = evalute_solution(sat, current_solution);
-                if(new_score == cls_num) return current_solution;
-                int delta = new_score - current_score;
-                if(delta >= 0){
-                    current_score = new_score;
-                }
-                else{
-                    double threshold = (-delta) / current_temperature;
-                    threshold = 1 + pow(M_E, threshold);
-                    threshold = 1 / threshold;
-                    double random_check = random() / RAND_MAX;
-                    if(random_check < threshold){
-                        current_score = new_score;
-                    }
-                    else{
-                        current_solution[k] = -current_solution[k];
-                    }
-                }
-            }
-            ++j;
+struct solution* random_solution(uint64_t size){
+    struct solution* retval = malloc(sizeof(struct solution));
+    retval->size = size;
+    retval->length = (size / 64);
+    retval->residue = size % 64; 
+    if(retval->residue) ++retval->length;
+    retval->value = malloc(retval->length * sizeof(uint64_t));
+    retval->presence = malloc(retval->length * sizeof(uint64_t));
+    uint64_t i;
+    for(i=0; i < retval->length; i++){
+        retval->value[i] = rand64();
+    }
+    for(i=0; i < retval->length - 1; i++){
+        retval->presence[i] = -1;
+    }
+    if(retval->residue){
+        for(i=0; i < retval->residue; i++){
+            retval->presence[retval->length - 1] <<= 1;
+            retval->presence[retval->length - 1] |= 1;
         }
-    }
-
-    return current_solution;
-}
-
-
-
-int evalute_solution(int** sat, int* solution){ 
-    int fitness = 0;
-    int var_num = sat[0][0]; 
-    int cls_num = sat[0][1];
-    int i,j;
-    for(i = 1; i <= cls_num; i++){
-        for(j = 1; j <= sat[i][0]; j++){
-            int literal = sat[i][j];
-            if(literal == solution[abs(literal)]){
-                ++fitness;
-                break;
-            }
-        }
-    }
-    return fitness;
-}
-
-int* random_solution(int var_num){
-    int* solution = malloc((var_num + 1) * sizeof(int));
-    solution[0] = var_num;
-    int i;
-    for (i = 1; i <= var_num; i++){
-        solution[i] = (rand() % 2) ? i : -i; 
-    }
-    return solution;
-}
-
-int** random_ksat(int var_num, int cls_num, int k){
-    if (k > var_num) return NULL;
-    int** ksat = malloc((cls_num + 1) * sizeof(int*));
-    ksat[0] = malloc(2 * sizeof(int));
-    ksat[0][0] = var_num; 
-    ksat[0][1] = cls_num;
-    int i,j;
-    for(i = 1; i <= cls_num; i++){
-        ksat[i] = malloc((k + 1) * sizeof(int));
-        ksat[i][0] = k;
-        int* set = calloc((var_num + 1), sizeof(int));
-        for(j = 1; j <= k; j++){
-            int to_set = 0;
-            do{
-                to_set = rand() % (var_num + 1);
-                if(set[to_set] == 0){
-                    set[to_set] = 1;
-                }
-                else{
-                    to_set = 0;
-                }
-            }while(to_set == 0);
-            ksat[i][j] = (rand() % 2) ? to_set : -to_set;
-        }
-        free(set);
-    }
-    return ksat;
-}
-
-void erase_solution(int* solution){
-    free(solution);
-}
-
-void erase_sat(int** sat){
-    int cls_num = sat[0][1];
-    int i;
-    for(i = 0; i <= cls_num; i++){
-        free(sat[i]);
-    }
-    free(sat);
-}
-
-void print_array(int* array){
-    printf("******************\n");
-    if(array){
-        printf("%d: ", array[0]);
-        int i;
-        for(i = 1; i <= array[0]; i++){
-            printf("%d, ", array[i]);
-        }
-        printf("\n");
     }
     else{
-        printf("NULL ARRAY\n");
+        retval->presence[retval->length - 1] = -1;
+    }
+    return retval;
+}
+
+struct clause* random_clause(uint64_t size){
+    struct clause* cls = (struct clause*)random_solution(size);
+    int i;
+    for(i = 0; i < cls->length; i++){
+        cls->presence[i] &= rand64();
+    }
+    return cls;
+}
+
+struct sat* random_sat(uint64_t cls_num, uint64_t var_num){
+    struct sat* retval = malloc(sizeof(struct sat));
+    retval->cls_num = cls_num;
+    retval->var_num = var_num;
+    retval->clauses = malloc(cls_num * sizeof(struct clause*));
+    int i;
+    for(i = 0; i < cls_num; i++){
+        retval->clauses[i] = random_clause(var_num);
+    }
+    return retval;
+}
+
+void print_solution(struct solution* sol){
+    printf("###|%4lu|%4lu|%4lu|###\n | ",sol->size,sol->length,sol->residue);
+
+    uint64_t i;
+    for(i=sol->length - 1; i > 0; --i){
+        printf("%016lx | ", sol->value[i]);
+    }
+    printf("%016lx | \n | ",sol->value[0]);
+
+    for(i=sol->length - 1; i > 0; --i){
+        printf("%016lx | ", sol->presence[i]);
+    }
+    printf("%016lx | \n",sol->presence[0]);
+}
+
+void print_clause(struct clause* cls){
+    print_solution((struct solution*) cls);
+}
+
+void print_sat(struct sat* formulae){
+    printf("-------------------------\n");
+    printf("@@@|%8lu|%8lu|@@@\n",formulae->cls_num,formulae->var_num);
+    printf("-------------------------\n");
+    uint64_t i;
+    for(i=0; i < formulae->cls_num; i++){
+        print_clause(formulae->clauses[i]);
     }
 }
 
-void print_formula(int** formula){
-    int i,j;
-    printf("##################\n");
-    printf("v: %d, c: %d\n",formula[0][0],formula[0][1]);
-    printf("##################\n");
-    int length = formula[0][1];
-    for(i = 1; i <= length; i++){
-        if(formula[i]){
-            printf("%3d(%3d): ", i, formula[i][0]);
-            for(j = 1; j<= formula[i][0]; j++){
-                printf("%3d, ", formula[i][j]);
-            }
-            printf("\n");
-        }
-        else{
-            printf("%3d(SAT):\n", i);
-        }
-    }
+uint64_t rand64(){
+    uint64_t retval = rand();
+    retval <<= 32;
+    retval |= rand();
+    return retval;
 }
